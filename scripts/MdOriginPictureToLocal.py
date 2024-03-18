06e61f7e802c6692fac29d1341bb8f950f02ce0f
zhiyong.tan <zhiyong.tan@yingzt.com>
2024-03-18 11:01:45 +0800

    add something

--- scripts/MdOriginPictureToLocal.py
+++ scripts/MdOriginPictureToLocal.py
@@ -1,29 +1,36 @@
-import requests;
+import requests
+import os
+
 
 def parseFileAndReplace(fileName):
     count = 0
     newLines = []
+
+    out_file = "./out/"
+    abs_image_path = fileName.split('.')[0] + "/"
+    if not os.path.exists(out_file + abs_image_path):
+        os.makedirs(out_file + abs_image_path)
+
     with open(fileName, "r") as f:
         lines = f.readlines()
         for line in lines:
-            if(line.startswith("![")):
+            if (line.startswith("![")):
                 count += 1
                 url = line.split("](")[1][:-2]
-                res = requests.get(url,params=None,verify=False)
-                imageFileName = "./Spring生命周期_image/img_" + str(count) + ".png"
-                with open(imageFileName, "wb") as f1:
+                print("url:", url)
+                res = requests.get(url, params=None, verify=False)
+                imageFileName = abs_image_path + "img_" + str(count) + ".png"
+                with open(out_file + imageFileName, "wb") as f1:
                     f1.write(res.content)
-                print("下载图片完成:"  + imageFileName)
+                print("下载图片完成:" + imageFileName)
                 line = line.replace(url, imageFileName)
                 print(line)
             newLines.append(line)
-    with open("NEW_"+fileName, "w+") as f:
+    with open(out_file + fileName, "w+") as f:
         for line in newLines:
             f.write(line)
-
-    print("总共图片数量:", count)
-
+    print("完成输出总共图片数量:", count)
 
 
 if __name__ == '__main__':
-    parseFileAndReplace("../docs/src/sourcecode/Spring生命周期.md")
+    parseFileAndReplace("谈谈伪共享.md")
